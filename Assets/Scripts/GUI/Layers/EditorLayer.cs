1c864608cf1d2867c443a2a5059c26bc54b17d8e
Poroh <meta.poroh.kun@gmail.com>
2018-04-14 15:08:51 +0300

    layers system

---
+++ Assets/Scripts/GUI/Layers/EditorLayer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Layers
+{
+    public class EditorLayer : LayerBase
+    {
+       
+    }
+}
---
+++ Assets/Scripts/GUI/Layers/MainMenuLayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Layers
+{
+    public class MainMenuLayer : LayerBase
+    {
+        public override HidingType Hiding { get { return HidingType.HideAll; } }
+
+        public void OnPlay()
+        {
+            LayersManager.FadeOut(0.5f, () =>
+            {
+                //LayersManager.Push<GameLayer>().Initialize();
+                LayersManager.FadeIn(0.5f, () =>
+                {
+                });
+            });
+        }
+    }
+}
